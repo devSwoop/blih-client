e8113dea54a3677f4d313ab62565ed8eb8e299e1
vincent <vincent.bertin@epitech.eu>
2018-01-27 15:44:00 +0100

    Vuetify

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-container>Login</v-container>
+</template>
+
+<script>
+export default {
+	name: 'login',
+
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+</style>
